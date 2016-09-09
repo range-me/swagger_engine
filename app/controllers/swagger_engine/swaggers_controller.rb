af207ba4dce53b3bdd398d2e521e4c2796275c9e
Peter Anderson <peter@peteranderson.me>
2016-09-09 11:10:49 +1000

    Retrieve JSON Swagger from URLs rather that files on disk; CSS tweak

--- app/controllers/swagger_engine/swaggers_controller.rb
+++ app/controllers/swagger_engine/swaggers_controller.rb
@@ -12,16 +12,21 @@ module SwaggerEngine
 
     def show
       respond_to do |format|
-        format.html { 
-          @swagger_json_url = swagger_path(params[:id], format: :json)
+        format.html {
+          @swagger_json_url = json_url(@json_files[params[:id].to_sym])
         }
-        format.json { 
+        format.json {
           send_file @json_files[params[:id].to_sym], type: 'application/json', disposition: 'inline'
         }
       end
     end
 
     private
+
+    def json_url path
+      "#{request.protocol}#{request.host}:#{request.port}#{path}"
+    end
+
     def load_json_files
       @json_files ||= SwaggerEngine.configuration.json_files || { default: "#{Rails.root}/lib/swagger_engine/swagger.json" }
     end
